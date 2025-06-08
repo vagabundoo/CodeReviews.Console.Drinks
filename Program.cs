7451e62549ff847daa8ac8620cfc0c6f3075e884
vagabundoo <evillamorm@gmail.com>
2025-06-09 01:14:47 +0200

    first prototype, with working request

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Spectre.Console;
+using System.Net.Http.Headers;
+
+using HttpClient client = new()
+{
+    BaseAddress = new Uri("https://www.thecocktaildb.com")
+};
+
+var response = await client.GetAsync("api/json/v1/1/random.php");
+
+Console.WriteLine(response);
+
+/*client.DefaultRequestHeaders.Accept.Clear();
+//client.DefaultRequestHeaders.Accept.Add(
+//    new MediaTypeWithQualityHeaderValue("www.thecocktaildb.com/api/json/v1/1/random.php"));
+//client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+await ProccessListDrinks(client);
+
+static async Task ProccessListDrinks(HttpClient client)
+{
+    var json = await client.GetStringAsync(
+        "www.thecocktaildb.com/api/json/v1/1/random.php");
+    System.Console.WriteLine(json);
+}
+*/
+Console.WriteLine("Welcome to Beach Bar");
+
+// mock data
+string[] drinkCategories = ["Alcoholic", "Non-aloholic"];
+
+string[] alcoholicDrinks = ["dark beer", "white beer"];
+string[] nonalcoholicDrinks = ["coffee", "tea"];
+
+var categoryChoice = AnsiConsole.Prompt(
+    new SelectionPrompt<string>()
+        .Title("Choose an category:")
+        .AddChoices(drinkCategories)
+);
+
+switch (categoryChoice)
+{
+    case "Alcoholic":
+        var detailChoice1 = AnsiConsole.Prompt(
+            new SelectionPrompt<string>()
+                .Title("Choose a drink")
+                .AddChoices(alcoholicDrinks)
+        );
+        AnsiConsole.MarkupLine($"You have chosen: [purple]{detailChoice1}[/]!");
+        break;
+    case "Non-aloholic":
+        var detailChoice2 = AnsiConsole.Prompt(
+            new SelectionPrompt<string>()
+                .Title("Choose a drink")
+                .AddChoices(nonalcoholicDrinks)
+        );
+        AnsiConsole.WriteLine($"You have chosen: {detailChoice2}!");
+        break;
+    default:
+        break;
+}
---
+++ RetrieveDrinks.cs
@@ -0,0 +1,6 @@
+using System.Net.Http.Headers;
+
+public class RetrieveDrinksInfo
+{
+    //new();    
+}
