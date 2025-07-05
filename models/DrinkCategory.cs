2475db18d26796537687f14ff7e973e0d498fa87
vagabundoo <evillamorm@gmail.com>
2025-07-05 23:05:36 +0200

    readded up to date files

--- models/DrinkCategory.cs
+++ models/DrinkCategory.cs
@@ -1,6 +1,7 @@
+using System.Collections.Generic;
 using System.Text.Json.Serialization;
 
-namespace drinksRequestsProject;
+namespace drinksRequestsProject.models;
 
 public class DrinkCategory
 {
